c2f5d613366783240abf8d301846ea6cb7e29eb1
Josh Craig <jcraig451@yahoo.com>
2020-03-07 21:04:02 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes here
+app.use(require('./routes/api'));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
